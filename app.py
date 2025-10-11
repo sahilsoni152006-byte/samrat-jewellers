7ce096a951fdea2b90a352e9d1f2bae654b56e00
sahilsoni152006-byte <sahilsoni152006@gmail.com>
2025-10-11 23:24:26 +0530

    Add Render deployment configuration

--- app.py
+++ app.py
@@ -719,5 +719,5 @@ def admin_toggle_product(product_id):
 
 if __name__ == '__main__':
     init_firebase()
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
