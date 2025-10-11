98fde99c885d5d5eb0d6696e0ae7fed1bb9895d0
Sahil Kumar <sahil157soni@gmail.com>
2025-10-11 23:12:21 +0530

    Add Railway deployment configuration

--- app.py
+++ app.py
@@ -719,4 +719,5 @@ def admin_toggle_product(product_id):
 
 if __name__ == '__main__':
     init_firebase()
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
