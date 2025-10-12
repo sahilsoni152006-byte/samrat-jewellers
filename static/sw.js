2da1ed6476343feef8ff12c063b8219377e31c2c
sahilsoni152006-byte <sahilsoni152006@gmail.com>
2025-10-12 15:04:55 +0530

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'samrat-jewellers-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/style.css',
+  '/static/js/script.js',
+  '/static/images/favicon/favicon.png',
+  '/static/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
